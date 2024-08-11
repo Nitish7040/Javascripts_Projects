ab0692ffb5a9f43b5dfd4e0825d958c4e155bd9f
Nitish pandey <153537473+Nitish7040@users.noreply.github.com>
2024-08-12 02:16:04 +0530

    add project 2,3 and 4

---
+++ Project_03_/script.js
@@ -0,0 +1,12 @@
+console.log("project 3");
+
+const clock =document.getElementById('clock')
+
+
+setInterval(function(){
+    let date = new Date()
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+    
+}, 1000)
+
